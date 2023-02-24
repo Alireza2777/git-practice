c2985e43e8c3b4b019cfb2256c4086cb8d8f29c5
Derrick Park <derrick8689@gmail.com>
2023-02-24 09:25:52 -0800

    Add some loop

--- main.swift
+++ main.swift
@@ -1,9 +1,11 @@
 print("Hello, Swift!")
-print("Hello, Swift!")
-print("Hello, Swift!")
+print("I am so excited for the weekend!")
 
 var a = 10
 var b = 10
 var c = 10
 print("a + b + c = \(a + b + c)")
 
+for i in 0..<10 {
+    print(i)
+}
